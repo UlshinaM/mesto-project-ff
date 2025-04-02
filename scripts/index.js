ed4901947dd888b04e2e7cccaf9d6ee5f4a4a821
Mariia Sunnikova <honey.born@mail.ru>
2025-04-02 14:41:33 +0300

    First code iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function addCardToPage(cardImage, cardTitle, deleteCard) {
+    const newCardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    newCardElement.querySelector('.card__image').src = cardImage;
+    newCardElement.querySelector('.card__title').textContent = cardTitle;
+
+    newCardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+    return newCardElement;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCardFunction(evt) {
+    let placeItem = evt.target.closest('.places__item');
+    placeItem.remove();
+};
+
 // @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+    let imageLink = initialCards[i].link;
+    let titleCard = initialCards[i].name;
+
+    placesList.append(addCardToPage(imageLink, titleCard, deleteCardFunction));
+};
